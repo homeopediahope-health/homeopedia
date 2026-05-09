3a47e8f9fc55948a4df7ed6c7c029337fe74e250
Dr.Shadab Khan <dr.shadabshomoeopathy@Mac.lan>
2026-05-10 03:20:07 +0530

    Add Google Analytics GA4 (G-PQSJBBY63K)

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
         <BottomNav />
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PQSJBBY63K"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-PQSJBBY63K');`
+          }}
+        />
+        <Script
           id="clarity-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
